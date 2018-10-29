47a1e953a33550811160502beda509e3e79b403c
Greg V <greg@unrelenting.technology>
2018-10-29 15:20:35 +0300

    Basic working screenshot
    for now just dumping surfaces as separate images

--- layered-screenshooter.cpp
+++ layered-screenshooter.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <fstream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <webp/encode.h>
 #include <wayland-client.h>
 #include "wldip-layered-screenshooter-client-protocol.h"
 #include "Screenshot_generated.h"
@@ -21,9 +27,33 @@ static const struct wl_registry_listener registry_listener = {
 
 static bool received = false;
 
-
-static void on_done(void *data, struct wldip_layered_screenshooter *shooter, struct wl_array *contents) {
+static void on_done(void *data, struct wldip_layered_screenshooter *shooter, int recv_fd) {
+	using namespace wldip::layered_screenshot;
 	received = true;
+	//lseek(recv_fd, 0, SEEK_SET);
+	struct stat recv_stat;
+	fstat(recv_fd, &recv_stat);
+	void *fbuf = mmap(nullptr, recv_stat.st_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, recv_fd, 0);
+	auto fshot = GetScreenshot(fbuf);
+	int counter = 0;
+	for (const auto *layer : *fshot->layers()) {
+		std::cout << "Layer" << std::endl;
+		for (const auto *surface : *layer->surfaces()) {
+			uint8_t *buf = const_cast<uint8_t*>(surface->contents()->Data());
+			// NOTE: pixman big-endian bgra == little endian rgba, don't touch
+			std::cout << "Surface " << counter << " w=" << surface->width() << " h=" << surface->height()
+				<< " x=" << surface->x() << " y=" << surface->y()
+				<< " buf " << buf[0] << buf[1] << buf[2] << buf[3]
+				<< std::endl;
+			uint8_t *webpbuf = nullptr;
+			size_t webpsiz = WebPEncodeRGBA(buf, surface->width(), surface->height(), surface->width() * 4, 98, &webpbuf);
+			char *fname = nullptr;
+			asprintf(&fname, "surface-%d.webp", counter++);
+			std::ofstream ofs(fname, std::ofstream::binary);
+			ofs.write(reinterpret_cast<char*>(webpbuf), webpsiz);
+		}
+	}
+	close(recv_fd);
 }
 
 static const struct wldip_layered_screenshooter_listener shooter_listener = { on_done };
@@ -47,6 +77,7 @@ int main(int argc, char *argv[]) {
 	wldip_layered_screenshooter_add_listener(shooter, &shooter_listener, nullptr);
 	wldip_layered_screenshooter_shoot(shooter);
 	while (!received) {
+		wl_display_dispatch(display);
 		wl_display_roundtrip(display);
 	}
 }
--- layered-screenshot.cpp
+++ layered-screenshot.cpp
@@ -1,24 +1,59 @@
 #include <iostream>
 #include <memory>
-//#include <unordered_map>
+#include <vector>
+#include <unordered_map>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 #include <compositor.h>
 #include "wldip-layered-screenshooter-server-protocol.h"
 #include "Screenshot_generated.h"
 
 struct ls_context {
-	struct weston_compositor *compositor;
+	const struct weston_compositor *compositor;
 
-	ls_context(struct weston_compositor *c) : compositor(c) { }
+	ls_context(const struct weston_compositor *c) : compositor(c) { }
 
 	ls_context(ls_context&&) = delete;
 };
 
 static void shoot(struct wl_client *client, struct wl_resource *resource) {
+	using namespace wldip::layered_screenshot;
 	auto *ctx = reinterpret_cast<struct ls_context*>(wl_resource_get_user_data(resource));
+	std::unordered_map<struct weston_layer*, std::vector<struct weston_view*>> layers;
 	struct weston_view *view;
 	wl_list_for_each(view, &ctx->compositor->view_list, link) {
-		weston_log("Surface role %s\n", weston_surface_get_role(view->surface));
+		layers[view->layer_link.layer].push_back(view);
 	}
+	flatbuffers::FlatBufferBuilder builder(32768);
+	std::vector<flatbuffers::Offset<Layer>> flayers;
+	for (const auto &kv : layers) {
+		std::vector<flatbuffers::Offset<Surface>> fsurfs;
+		for (const auto *view : kv.second) {
+			uint8_t *buf = nullptr;
+			int cw = 0, ch = 0;
+			weston_surface_get_content_size(view->surface, &cw, &ch);
+			//size_t len = view->surface->width * view->surface->height * 4;
+			size_t len = cw * ch * 4;
+			auto contents = builder.CreateUninitializedVector<uint8_t>(len, &buf);
+			int ccr = weston_surface_copy_content(view->surface, reinterpret_cast<void*>(buf),
+					len, 0, 0, cw, ch);
+			fsurfs.push_back(CreateSurface(builder,
+						view->geometry.x, view->geometry.y,
+						cw, ch,
+						Layout_Pixman_A8B8G8R8,
+						contents));
+		}
+		flayers.push_back(CreateLayer(builder, builder.CreateVector(fsurfs), 0));
+	}
+	// TODO find size
+	builder.Finish(CreateScreenshot(builder, 1366, 768, builder.CreateVector(flayers)));
+	int fd = shm_open(SHM_ANON, O_RDWR | O_CREAT, 0644);
+	ftruncate(fd, builder.GetSize());
+	write(fd, builder.GetBufferPointer(), builder.GetSize());
+	wldip_layered_screenshooter_send_done(resource, fd);
+	close(fd);
 }
 
 static struct wldip_layered_screenshooter_interface ls_impl = { shoot };
